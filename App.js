afc18522359dca68f92acb2b64e19e7c57a7bab0
Matthew Zhang <mattzhang989@gmail.com>
2021-01-14 01:12:17 -0800

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default function App() {
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Smile Translator</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text1 => setText1(text1)}
+        defaultValue={text1}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text1.split(' ').map((word) => word && ':)').join(' ')}
+      </Text>
+      <Text>Frown Translator</Text>
+      <StatusBar style="auto" />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text2 => setText2(text2)}
+        defaultValue={text2}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text2.split(' ').map((word) => word && ':(').join(' ')}
+      </Text>
     </View>
   );
 }
